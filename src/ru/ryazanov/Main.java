c03de9828478f022fbc766ae742d28ef5d81e8e1
Ryazan <ryazangaming@gmail.com>
2019-07-25 21:29:39 +0500

    Init project

---
+++ src/ru/ryazanov/Main.java
@@ -0,0 +1,8 @@
+package ru.ryazanov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/ru/ryazanov/Search.java
@@ -0,0 +1,7 @@
+package ru.ryazanov;
+
+public class Search {
+    public static int binarySearch(int[] list, int item) {
+        return item;
+    }
+}
